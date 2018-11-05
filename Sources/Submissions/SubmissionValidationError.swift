1c1b66f5356be7685ae3087044e5af6791f7f6b2
Eric Thorpe <eric@sparklabs.com>
2018-11-05 16:34:56 +1100

    Adds EncodeEx on SubmissionsValidationError with extra information for json

--- Sources/Submissions/SubmissionValidationError.swift
+++ Sources/Submissions/SubmissionValidationError.swift
@@ -14,11 +14,48 @@ private struct ErrorResponse: Encodable {
     /// Validation reason(s) per field.
     let validationErrors: [String: [String]]
 }
+private struct ErrorResponseEx: Encodable {
+    /// Always set `error` to `true` in response.
+    let error = true
+
+    /// Reason for the error.
+    let reason = "One or more fields failed to pass validation."
+
+    /// Validation reason(s) per field.
+    let validationErrors: [String: [String:String]]
+}
 
 extension SubmissionValidationError: ResponseEncodable {
     /// See `ResponseEncodable`
     public func encode(for req: Request) throws -> Future<Response> {
         let errorResponse = try ErrorResponse(validationErrors: req.fieldCache().errors)
+
+        let response = try req.response(
+            http: .init(
+                status: .unprocessableEntity,
+                body: HTTPBody(data: JSONEncoder().encode(errorResponse))
+            )
+        )
+        response.http.headers.replaceOrAdd(
+            name: .contentType,
+            value: "application/json; charset=utf8"
+        )
+
+        return req.future(response)
+    }
+    /// As above, except adds labels and types error, only returns first error though
+    public func encodeEx(for req: Request) throws -> Future<Response> {
+        let fieldCache = try req.fieldCache()
+
+        var validationErrors: [String:[String:String]] = [:]
+        for (id, errors) in fieldCache.errors {
+            var errd:[String:String] = [:]
+            errd["error"] = errors[0]
+            errd["label"] = fieldCache[valueFor: id]?.label
+            validationErrors[id] = errd
+        }
+
+        let errorResponse = ErrorResponseEx(validationErrors: validationErrors)
 
         let response = try req.response(
             http: .init(
